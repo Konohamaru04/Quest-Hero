f94cb17e4187932457edb35a02bfcb2fe77c6cf9
Konohamaru04 <41485055+Konohamaru04@users.noreply.github.com>
2019-10-25 17:58:50 +0530

    implemented diedUI

---
+++ Triangle Game 2.0/Assets/Player/scripts/diedUIbuttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diedUIbuttons : MonoBehaviour
+{
+    public GameObject diedUI;
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+}
